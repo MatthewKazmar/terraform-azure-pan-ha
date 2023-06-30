415daced7b2ccbf1b89c23a0c7ae067c5da6c3ad
Matthew Kazmar <matt@kazmar.org>
2023-06-30 10:37:24 -0500

    keys

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "azurerm_linux_virtual_machine" "fw" {
   admin_username = var.user
   admin_password = "mytemppassword"
 
-  network_interface_ids = [for nic in each.value.nic : azurerm_network_interface.nic[nic].id]
+  network_interface_ids = [for nic in keys(each.value.nic) : azurerm_network_interface.nic[nic].id]
 
   availability_set_id = contains(local.az_regions, azurerm_resource_group.rg.location) ? null : one(azurerm_availability_set.avset).id
   zone                = contains(local.az_regions, azurerm_resource_group.rg.location) ? each.value.az : null
