2fae893860739e4ad88deb2141bdedd7e8713d2e
Matthew Kazmar <matt@kazmar.org>
2024-05-06 13:27:50 -0500

    subnet name

--- nsg.tf
+++ nsg.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_group" "this" {
   for_each = local.subnets
 
-  name                = each.value
+  name                = each.key
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
