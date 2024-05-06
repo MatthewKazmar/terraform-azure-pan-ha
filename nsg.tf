6efe43c627e8fbb7763d755543e57fe755774173
Matthew Kazmar <matt@kazmar.org>
2024-05-06 15:06:31 -0500

    ranges

--- nsg.tf
+++ nsg.tf
@@ -75,7 +75,7 @@ resource "azurerm_network_security_rule" "allow_in_ipsec" {
   access                      = "Allow"
   protocol                    = "Udp"
   source_port_range           = "*"
-  destination_port_range      = "500, 4500"
+  destination_port_ranges     = [500, 4500]
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.this.name
